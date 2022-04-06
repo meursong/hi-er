36bbb04ea0d32c552b64bb64bd86861326f9ea5e
meursong <94438960+meursong@users.noreply.github.com>
2022-04-06 23:31:28 +0900

    su/0406/payment page design done, payment page data receiving done

--- Hi-er/src/main/java/com/mulcam/hier/controller/PostController.java
+++ Hi-er/src/main/java/com/mulcam/hier/controller/PostController.java
@@ -191,10 +191,12 @@ public class PostController {
 	
 	//결제하기 테스트
 	@PostMapping("/payment")
-	public ModelAndView pay(Product product, @RequestParam String abc) { //mav로
+	public ModelAndView pay(Product product, @RequestParam String paymentPkg) { //mav로
 		ModelAndView mav = new ModelAndView("payment");
 		System.out.println(product.getB_commercial());
-		System.out.println(abc);
+		System.out.println(paymentPkg);
+		System.out.println(product.getS_commercial());
+		System.out.println(product.getTitle());
 		// 
 		// abc가 b면 >> model 에다가 b를 통째로 넣어요...
 		// abc가 s면 >> model 에다가 s를 통째로 넣어요...
