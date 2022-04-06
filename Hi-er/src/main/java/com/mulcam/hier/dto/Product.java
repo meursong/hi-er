161c775d508ac1c37e3b12a09d58037cdf85a726
meursong <94438960+meursong@users.noreply.github.com>
2022-04-06 16:52:56 +0900

    su/0406/error fixing after integ

--- Hi-er/src/main/java/com/mulcam/hier/dto/Product.java
+++ Hi-er/src/main/java/com/mulcam/hier/dto/Product.java
@@ -1,52 +1,5 @@
 package com.mulcam.hier.dto;
 
-public class Product {
-	private String nickname;
-	private String content;
-	private int work_available;
-	private int commission;
-	private int report;
-	private String title;
-	
-	public String getNickname() {
-		return nickname;
-	}
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-	public String getProduct_content() {
-		return content;
-	}
-	public void setProduct_content(String product_content) {
-		this.content = product_content;
-	}
-	public int getWork_available() {
-		return work_available;
-	}
-	public void setWork_available(int work_available) {
-		this.work_available = work_available;
-	}
-	public int getCommission() {
-		return commission;
-	}
-	public void setCommission(int commission) {
-		this.commission = commission;
-	}
-	public int getReport() {
-		return report;
-	}
-	public void setReport(int report) {
-		this.report = report;
-	}
-	
-	public Product() {}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
 
 import java.time.LocalDate;
 
@@ -602,4 +555,5 @@ public class Product {
 		this.createdAt = createdAt;
 	}
 	
+
 }
