66b4aa12cd0695f06b89119527a166e68abeba29
meursong <94438960+meursong@users.noreply.github.com>
2022-04-13 01:08:08 +0900

    su/0413/Orders controller,dto,service added

--- Hi-er/src/main/java/com/mulcam/hier/controller/PaymentController.java
+++ Hi-er/src/main/java/com/mulcam/hier/controller/PaymentController.java
@@ -1,10 +1,32 @@
 package com.mulcam.hier.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody; 
+
+import com.mulcam.hier.dto.Orders;
+import com.mulcam.hier.service.OrderService;
 
 @Controller 
 public class PaymentController {
+	
+	@Autowired
+	OrderService orderService;
+	
 	@GetMapping("/iamport") public String iamport(){ 
 		return "index"; 
+	}
+	
+	@ResponseBody
+	@PostMapping("/order") 
+	public int order(Orders order){ 
+		int orderNumber = 0;
+		try {
+			orderNumber = orderService.makeOrder(order) ;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return orderNumber; 
 	}
 }
--- Hi-er/src/main/java/com/mulcam/hier/dto/Orders.java
+++ Hi-er/src/main/java/com/mulcam/hier/dto/Orders.java
@@ -4,62 +4,76 @@ import java.sql.Date;
 
 public class Orders {
 	private int order_id;
-	private String status;
+	private int order_status;
 	private int buyer_id;
 	private int seller_id;
 	private int product_id;
 	private Date createdAt;
+	private String order_pkg;
+	private int delivery_date;
+	private int order_qty;
+	private int order_price;
 	
-	public Orders() {}
-
 	public int getOrder_id() {
 		return order_id;
 	}
-
 	public void setOrder_id(int order_id) {
 		this.order_id = order_id;
 	}
-
-	public String getStatus() {
-		return status;
+	public int getOrder_status() {
+		return order_status;
 	}
-
-	public void setStatus(String status) {
-		this.status = status;
+	public void setOrder_status(int order_status) {
+		this.order_status = order_status;
 	}
-
 	public int getBuyer_id() {
 		return buyer_id;
 	}
-
 	public void setBuyer_id(int buyer_id) {
 		this.buyer_id = buyer_id;
 	}
-
 	public int getSeller_id() {
 		return seller_id;
 	}
-
 	public void setSeller_id(int seller_id) {
 		this.seller_id = seller_id;
 	}
-
 	public int getProduct_id() {
 		return product_id;
 	}
-
 	public void setProduct_id(int product_id) {
 		this.product_id = product_id;
 	}
-
 	public Date getCreatedAt() {
 		return createdAt;
 	}
-
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
-	
+	public String getOrder_pkg() {
+		return order_pkg;
+	}
+	public void setOrder_pkg(String order_pkg) {
+		this.order_pkg = order_pkg;
+	}
+	public int getDelivery_date() {
+		return delivery_date;
+	}
+	public void setDelivery_date(int delivery_date) {
+		this.delivery_date = delivery_date;
+	}
+	public int getOrder_qty() {
+		return order_qty;
+	}
+	public void setOrder_qty(int order_qty) {
+		this.order_qty = order_qty;
+	}
+	public int getOrder_price() {
+		return order_price;
+	}
+	public void setOrder_price(int order_price) {
+		this.order_price = order_price;
+	}
 	
 	
 }
